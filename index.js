7d61e37145cc2614e44b9e90ab9c615650121148
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-12 20:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-
+var animal = 'dog'
+animal = 'cat'
 
 function myAnimal() {
-  var animal = 'dog'
   return animal
-
-  function yourAnimal() {
-    // How can we make sure that this function
-    // and the above function both pass?
-    // P.S.: You can't just hard-code 'cat' below
-    return animal
-  }
-
-}
 }
 
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  return animal
+}
 
 function add2(n) {
   const two = 2
@@ -32,4 +29,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
 
-funkyFunction()
+
